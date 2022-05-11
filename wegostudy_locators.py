705031dd211b590984dd36277c4e82af75cc40fe
david wook huang <87260729+DavidWookH@users.noreply.github.com>
2022-05-11 13:19:13 -0700

    Update wegostudy_locators.py
    valid email/password inserted

--- wegostudy_locators.py
+++ wegostudy_locators.py
@@ -7,3 +7,7 @@ fake = Faker(locale=['en_CA','en_US'])
 app = 'WeGoStudy'
 wegostudy_url = 'http://34.233.225.85/'
 wegostudy_home_page_title = 'WeGoStudy'
+
+#---------[LOG IN]  valid email and password -------
+login_email = 'chris.velasco78@gmail.com'
+login_password = 'cctb123'
